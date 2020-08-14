029b0295c6596e2785ce75ff64d91233933e2cd3
Paul <prose0021@gmail.com>
2020-08-14 11:07:57 -0700

    capacity bar

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './src/App';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<p>Working</p>, root);
+    ReactDOM.render(<App />, root);
 })
