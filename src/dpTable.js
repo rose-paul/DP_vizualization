8b57caa038282347127f80ea756e55b191e74020
Paul <prose0021@gmail.com>
2020-08-16 14:39:49 -0700

    table dimensions

--- src/dpTable.js
+++ src/dpTable.js
@@ -2,30 +2,33 @@ import React, { useState, useEffect } from "react";
 
 
 const DynamicProgrammingTable = ({ numRows, numCols }) => {
-    const dpTable = new Array(numRows).fill().map( el => new Array(numCols).fill(0));
+    const dpTable = new Array(numRows).fill().map( el => new Array(numCols+1).fill(0));
     return (
-      <table>
-        <thead>DP Table</thead>
-        <tbody>
-          {dpTable[0].map((_, i) => (
-            <th>{i}</th>
-          ))}
-          {dpTable.map((row) => {
-            return (
-              <tr>
-                {row.map((val, idx) => {
-                  if (idx === 0) {
-                    return <th>{idx}</th>;
-                  } else {
-                    return <td>{val}</td>;
-                  }
+        <div>
+            <h3>Items (left), Capacity (top)</h3>
+            <table>
+              <thead>
+                  <tr>
+                      <th></th>
+                {dpTable[0].map((_, i) => (
+                  <th>{i}</th>
+                ))}
+                  </tr>
+              </thead>
+              <tbody>
+                {dpTable.map( (row, idx) => {
+                  return (
+                    <tr>
+                      <th>{idx}</th>
+                      {
+                          row.map(val => <td>{val}</td>)
+                      }
+                    </tr>
+                  );
                 })}
-              </tr>
-            );
-          })}
-        </tbody>
-        <tfoot>Done</tfoot>
-      </table>
+              </tbody>
+            </table>
+        </div>
     );
 }
 
